91b5d19a67be13d2786c31a4e80b192193a04607
ketherball <121590283+ketherball@users.noreply.github.com>
2023-10-19 16:42:45 -0400

    feat: style search bar

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,11 +1,55 @@
+import { useState } from "react";
 import "./Main.css";
+import searchIcon from "./search.png";
 
 function Main() {
     return(
         <main>
-            Main
+            <SearchBar/>
         </main>
     );
 }
 
 export default Main;
+
+function SearchBar() {
+
+    const [input, setInput] = useState('');
+
+    function handleChange(e) {
+        setInput(e.target.value);
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        setInput('');
+    }
+
+    return(
+        <form onSubmit={handleSubmit}>
+            <div className="searchBar">
+                <button>
+                    <img 
+                        src={searchIcon} 
+                        alt="Search Icon"
+                        height={20}
+                    />
+                </button>
+                <label 
+                    htmlFor="country-search"
+                    style={{display: "none"}}
+                >
+                    Search for a country
+                </label>
+                <input
+                    type="text"
+                    id="country-search"
+                    name="country-search"
+                    placeholder="Search for a country..."
+                    value={input}
+                    onChange={handleChange}
+                />
+            </div>
+        </form>
+    )
+}
