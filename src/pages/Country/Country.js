8fc5fd2823b27c8c9613927ea5dfd0dfd151831d
ketherball <121590283+ketherball@users.noreply.github.com>
2023-10-20 11:46:51 -0400

    feat: clean up responsive design

--- src/pages/Country/Country.js
+++ src/pages/Country/Country.js
@@ -45,35 +45,37 @@ function Country(props) {
 
     return(
         <div className="country">
-            <div style={{flex: "1"}}>
-                <button style={{display: "block"}} onClick={handleClick}>Back</button>
-                <img src={flags.png} alt={`${name.common} flag`}/>
-            </div>
-            <div style={{flex: "1"}}>
-                <div>
-                    <h2>{name.common}</h2>
-                    <div className="flexBox">
-                        <section>
-                            <p>Native Name: <span>{getNativeName(name.nativeName)}</span></p>
-                            <p>Population: <span>{population}</span></p>
-                            <p>Region: <span>{region}</span></p>
-                            <p>Sub Region: <span>{subregion}</span></p>
-                            <p>Capital: <span>{capital}</span></p>
-                        </section>
-                        <section>
-                            <p>Top Level Domain: <span>{tld}</span></p>
-                            <p>Currencies: <span>{getCurrencies(currencies)}</span></p>
-                            <p>Languages: <span>{getLanguages(languages)}</span></p>
-                        </section>
-                    </div>
+            <button style={{display: "block"}} onClick={handleClick}>Back</button>
+            <div className="flexBox__outer">
+                <div className="flexBox__outer--left" style={{flex: "1"}}>
+                    <img src={flags.png} alt={`${name.common} flag`}/>
                 </div>
-                {borders && 
-                <section>
-                    <p>Border Countries:</p>
-                    {borders.map(border => {
-                        return <span key={border} className="borderTag">{border}</span>
-                    })}
-                </section>}
+                <div className="flexBox__outer--right" style={{flex: "1"}}>
+                    <div>
+                        <h2>{name.common}</h2>
+                        <div className="flexBox__inner">
+                            <section className="flexBox__inner--left">
+                                <p>Native Name: <span>{getNativeName(name.nativeName)}</span></p>
+                                <p>Population: <span>{population}</span></p>
+                                <p>Region: <span>{region}</span></p>
+                                <p>Sub Region: <span>{subregion}</span></p>
+                                <p>Capital: <span>{capital}</span></p>
+                            </section>
+                            <section className="flexBox__inner--right">
+                                <p>Top Level Domain: <span>{tld}</span></p>
+                                <p>Currencies: <span>{getCurrencies(currencies)}</span></p>
+                                <p>Languages: <span>{getLanguages(languages)}</span></p>
+                            </section>
+                        </div>
+                    </div>
+                    {borders && 
+                    <section>
+                        <p>Border Countries:</p>
+                        {borders.map(border => {
+                            return <span key={border} className="borderTag">{border}</span>
+                        })}
+                    </section>}
+                </div>
             </div>
         </div>
     )
