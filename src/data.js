0b6b92e7a3bc7e7f83cd07e35a19cd358d67bb27
ketherball <121590283+ketherball@users.noreply.github.com>
2023-10-20 14:27:24 -0400

    Updates

--- src/data.js
+++
@@ -1,8 +0,0 @@
-
-async function getData() {
-    const response = await fetch("https://restcountries.com/v3.1/all");
-    const countries = await response.json();
-    return countries;
-};
-
-export { getData };
